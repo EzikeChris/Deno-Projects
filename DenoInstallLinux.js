66405bfac4de29d788599372d9b8ccb490b1a00b
ezikechris <christophermbe1009@gmail.com>
2021-01-14 12:09:13 +0100

    initial committ

---
+++ DenoInstallLinux.js
@@ -0,0 +1,41 @@
+/// INSTALLATION OF DENO ON LINUX OS ////
+
+
+/*
+Installing Deno by running the following command from it's official website
+
+curl -fsSL https://deno.land/x/install/install.sh | sh
+
+
+
+After installing it update .bashrc file :
+
+
+COPY AND PASTE THE BELOW COMMAND :
+sudo nano .bashrc
+
+
+
+then add these two lines in the file:
+
+ 
+
+
+export DENO_INSTALL="/$HOME/.deno"
+ export PATH="$DENO_INSTALL/bin:$PATH"
+
+
+
+finally run the following command 
+
+
+
+
+source ~/.bashrc
+
+after that run deno by typing this command in terminal deno
+ 
+
+
+!!!!1 GOODLUCK !!!!!1
+*/
